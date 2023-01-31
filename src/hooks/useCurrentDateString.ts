913d81db83c995dccd1593ff5c1087790ee6d909
Antônio Hugo <hugorplobo@gmail.com>
2023-01-31 17:11:26 -0300

    feat: add custom hooks

---
+++ src/hooks/useCurrentDateString.ts
@@ -0,0 +1,8 @@
+export default function useCurrentDateString() {
+  const date = new Date();
+
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
+    2,
+    "0"
+  )}-${date.getDate()}`;
+}
---
+++ src/hooks/useGuesses.ts
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+
+export interface GuessesState {
+  remaining: number;
+  history: string[];
+  hasWon: boolean;
+  hasLost: boolean;
+  response: string;
+  pushHistory: (value: string) => void;
+  decreaseGuesses: () => void;
+  win: () => void;
+  lose: () => void;
+  setResponse: (value: string) => void;
+}
+
+export const useGuesses = create<GuessesState>((set) => ({
+  remaining: 5,
+  history: [],
+  hasWon: false,
+  hasLost: false,
+  response: "",
+  pushHistory: (value) =>
+    set((state) => ({ history: [value, ...state.history] })),
+  decreaseGuesses: () =>
+    set((state) => {
+      if (state.remaining <= 1) {
+        state.lose();
+      }
+
+      return { remaining: state.remaining - 1 };
+    }),
+  win: () => set((_) => ({ hasWon: true })),
+  lose: () => set((_) => ({ hasLost: true })),
+  setResponse: (value) => set((_) => ({ response: value })),
+}));
