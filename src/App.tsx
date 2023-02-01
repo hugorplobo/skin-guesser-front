d1ed11f1d487bc6beccba9033d8c69d848ceea68
Antônio Hugo <hugorplobo@gmail.com>
2023-02-01 19:17:38 -0300

    refactor: move game end feedback

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,19 @@ import GuessesRemaining from "./components/GuessesRemaining";
 import Input from "./components/Input";
 import { useGuesses } from "./hooks/useGuesses";
 import useSWRImmutable from "swr/immutable";
-import getSkinName from "./utils/getSkinName";
 import GuessesHistory from "./components/GuessesHistory";
 import { config } from "./config";
+import GameEnded from "./components/GameEnded";
+import { useEffect } from "react";
 
 export default function App() {
   const date = useCurrentDateString();
 
   const hasWon = useGuesses(state => state.hasWon);
   const hasLost = useGuesses(state => state.hasLost);
-  const remaining = useGuesses(state => state.remaining);
   const skinName = useGuesses(state => state.response);
   const setSkin = useGuesses(state => state.setResponse);
+  const setGameDate = useGuesses(state => state.setGameDate);
 
   const url = `${config.apiUrl}/game?date=${date}`;
   const { data } = useSWRImmutable<{ skin_name: string }>(url, async () => {
@@ -27,28 +28,20 @@ export default function App() {
     setSkin(data.skin_name);
   }
 
+  useEffect(() => {
+    setGameDate(date);
+  }, []);
+
   return (
-    <div className="flex w-full h-screen text-white items-center p-4 flex-col justify-between">
+    <div className="flex w-full min-h-screen text-white items-center p-4 flex-col justify-between">
       <div className="flex w-full items-center flex-col">
         <h1 className="font-bold text-2xl">Skin Guesser</h1>
         <CanvasImage 
           url={url}
           className="w-full max-w-lg m-4 shadow-lg" 
         />
-        { hasLost ? (
-          <>
-            <p>Você perdeu, a skin era:</p>
-            <p className="text-2xl font-bold text-red-500">
-              { getSkinName(skinName) }
-            </p>
-          </>
-        ) : hasWon ? (
-          <>
-            <p>Parabéns! Você acertou em { 5 - remaining + 1 } tentativa{ 5 - remaining + 1 === 1 ? "" : "s" }</p>
-            <p className="text-2xl font-bold text-green-500">
-              { getSkinName(skinName) }
-            </p>
-          </>
+        { hasLost || hasWon ? (
+          <GameEnded />
         ) : (
           <>
             <GuessesRemaining />
@@ -57,7 +50,7 @@ export default function App() {
           </>
         ) }
       </div>
-      <footer className="text-xs text-center max-w-lg text-neutral-500 relative bottom-4">
+      <footer className="text-xs text-center max-w-lg text-neutral-500">
         Skin Guesser is not endorsed by Riot Games and does not reflect the
         views or opinions of Riot Games or anyone officially involved in
         producing or managing Riot Games properties. Riot Games and all
